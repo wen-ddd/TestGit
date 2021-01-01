9aff8978a29f41af45e80242b51add6a3f2ace2d
wen-ddd <22589695@qq.com>
2021-01-01 10:41:14 +0800

    first commit

---
+++ src/main/java/helloController/controller.java
@@ -0,0 +1,13 @@
+package helloController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+
+    @RequestMapping("/index")
+    public String hello() {
+        return "index";
+    }
+}
